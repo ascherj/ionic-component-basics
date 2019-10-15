66da40bc6c223bf3bec1ca77b843e2c1bae4a3b8
jake-ascher <jascher@diversifiedus.com>
2019-10-15 11:21:28 -0400

    Add an event listener

---
+++ app.js
@@ -0,0 +1,8 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmBtn = document.querySelector('#btn-confirm');
+
+reasonInput.addEventListener('click', () => {
+    console.log('It works!');
+});
