947551a2f97cdce6dbac2b788f1600645a120e57
jake-ascher <jascher@diversifiedus.com>
2019-10-15 11:27:49 -0400

    Validate user input

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 
-reasonInput.addEventListener('click', () => {
-    console.log('It works!');
+confirmBtn.addEventListener('click', () => {
+    const enteredReason = reasonInput.value;
+    const enteredAmount = amountInput.value;
+
+    if (
+        enteredReason.trim().length <= 0 ||
+        enteredAmount.trim().length <= 0 ||
+        enteredAmount <= 0
+    ) {
+        return;
+    }
+
+    console.log(enteredReason, enteredAmount);
 });
